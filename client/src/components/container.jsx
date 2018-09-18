c5abade80935dbf6d30c7b78a6cf812866bb95f9
Yon Taek Chung <yontaekc@gwu.edu>
2018-09-18 16:27:06 -0700

    fixed more

--- client/src/components/container.jsx
+++ client/src/components/container.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 import styles from '../../src/search.css';
 import MoreThanFive from './moreThanFive.jsx';
 import LessThanFive from './lessThanFive.jsx';
+import Button from './button.jsx';
 
-var Container = ({ pictures, onClickHandler }) => {
+var Container = ({ pictures, onClickHandler, buttonHandler }) => {
   return (
     <div className={styles.container}>
       {pictures.length > 5 ? (
@@ -11,6 +12,7 @@ var Container = ({ pictures, onClickHandler }) => {
       ) : (
         <LessThanFive pictures={pictures} onClickHandler={onClickHandler} />
       )}
+      <Button pictures={pictures} buttonHandler={buttonHandler} />
     </div>
   );
 };
